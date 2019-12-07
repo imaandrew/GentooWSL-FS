ea7b9a5541759bea1222d1cd6bfb7c678ff4b3c7
Andrew Fehr <andrewfehr@outlook.com>
2019-12-07 12:10:15 -0500

    First commit!

---
+++ env.sh
@@ -0,0 +1,2 @@
+STAGE3=$( curl --silent http://distfiles.gentoo.org/releases/amd64/autobuilds/latest-stage3-amd64-nomultilib.txt | awk '{print $1;}' | grep -v "^#" )
+GTOO_URL="http://distfiles.gentoo.org/releases/amd64/autobuilds/${STAGE3}"
