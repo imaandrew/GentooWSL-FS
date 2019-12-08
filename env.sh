5698fb8c2ca7b26dcdb3794309b1b35775d677a7
Andrew Fehr <andrewfehr@outlook.com>
2019-12-07 20:41:25 -0500

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,2 +1,3 @@
 STAGE3=$( curl --silent http://distfiles.gentoo.org/releases/amd64/autobuilds/latest-stage3-amd64-nomultilib.txt | awk '{print $1;}' | grep -v "^#" )
 GTOO_URL="http://distfiles.gentoo.org/releases/amd64/autobuilds/${STAGE3}"
+TAR=$( echo $STAGE3 | awk -F'/' '{print $2}' )
