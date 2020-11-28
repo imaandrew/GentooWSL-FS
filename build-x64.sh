4853dce91f8bf83ef491f62acb5b85505020c74d
Andrew Fehr <andrewfehr@outlook.com>
2020-11-27 21:49:25 -0500

    Fix and update build script

--- build-x64.sh
+++ build-x64.sh
@@ -1,67 +1,66 @@
-sudo apt update -q
-sudo apt install -y -q curl wget xz-utils sudo pv
-
+# Setup environment and download files
 chmod +x env.sh
 . env.sh
 mkdir rootfs
 wget ${GTOO_URL}
-sudo tar -xf ${TAR} -C rootfs
-sudo cp -f wsl.conf rootfs/etc
-sudo cp -f resolv.conf rootfs/etc
-sudo cp -f make.conf rootfs/etc/portage
-sudo cp -f cpu.sh rootfs
-
+sudo tar -xpf ${TAR} -C rootfs --xattrs-include='*.*' --numeric-owner
 cd rootfs
-sudo mount -t proc proc proc/
-sudo mount --bind /sys sys
+sudo cp -f ../wsl.conf ./etc
+sudo cp -f ../make.conf ./etc/portage
+sudo cp -f ../cpu.sh ./
+
+# Mount directories and setup chroot
+sudo mount --types proc /proc ./proc
+sudo mount --rbind /sys ./sys
+sudo mount --make-rslave ./sys
+sudo mount --rbind /dev ./dev
+sudo mount --make-rslave ./dev
+sudo rm -rf /dev/shm
+sudo mkdir /dev/shm
+sudo mount --types tmpfs --options nosuid,nodev,noexec shm /dev/shm
+sudo chmod 1777 /dev/shm
+sudo mount --rbind /dev/shm/ ./dev/shm
+sudo chmod 1777 ./dev/shm/
 sudo chroot . sh cpu.sh
-sudo chroot . mkdir --parents etc/portage/repos.conf
-sudo chroot . cp usr/share/portage/config/repos.conf etc/portage/repos.conf/gentoo.conf
+sudo chroot . mkdir --parents ./etc/portage/repos.conf
+sudo chroot . cp ./usr/share/portage/config/repos.conf ./etc/portage/repos.conf/gentoo.conf
 sudo chroot . emerge-webrsync
-sudo chroot . emerge --oneshot -a n sys-devel/gcc
 
+# More portage stuff
 sudo chroot . emerge --update --deep --with-bdeps=y --newuse @world
-sudo chroot . emerge --oneshot -a n app-portage/mirrorselect
-sudo chmod -R a+rw etc
-sudo chroot . mirrorselect -s3 -o >> etc/portage/repos.conf/gentoo.conf
-sudo chroot . emerge --oneshot --usepkg=n -a n sys-devel/libtool
-sudo chroot . emerge --verbose --update --deep --newuse -a n @world
-sudo mkdir etc/portage/profile
-sudo chmod -R a+rw etc
-sudo echo "sys-apps/portage -ipc" > etc/portage/profile/package.use.force
-sudo echo 'ACCEPT_LICENSE="-* @FREE linux-fw-redistributable no-source-code"' >> etc/portage/make.conf
+sudo chroot . emerge -a n app-portage/mirrorselect
+echo 'ACCEPT_LICENSE="-* @FREE linux-fw-redistributable no-source-code"' | sudo tee -a ./etc/portage/make.conf
+sudo chroot . mirrorselect -s3
 
-sudo chroot . emerge -a n app-admin/sudo
-sudo chmod -R a+rw etc
-sudo cp etc/sudoers ../sudoers
-sudo chmod a+rw ../sudoers
-sudo echo "%wheel ALL=(ALL) ALL" >> ../sudoers
-sudo visudo -cf ../sudoers
-sudo cp ../sudoers etc/sudoers
-sudo chroot . emerge -a n app-shells/bash-completion
-sudo chroot . echo 'en_US.UTF-8 UTF-8' >> etc/locale.gen
+# Sudo n stuff
+sudo chroot . emerge -a n app-admin/sudo app-shells/bash-completion
+sudo sed -i -e 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' ./etc/sudoers
+sudo sed -i -e 's/pam_unix.so try_first_pass use_authtok nullok sha512 shadow/pam_unix.so nullok sha512 shadow/' ./etc/pam.d/system-auth
+sudo sed -ie "s/\(.*passwdqc.*\)/#\1/" ./etc/pam.d/system-auth
+
+# Locale stuff
+echo 'en_US.UTF-8 UTF-8' | sudo tee -a ./etc/locale.gen
 sudo chroot . locale-gen
-sudo chmod -R a+rw etc
-sudo chroot . echo 'LANG="en_US.UTF-8"' >> etc/env.d/02locale
-sudo chroot . echo 'LC_COLLATE="C"' >> etc/env.d/02locale
-sudo cp -f ../resolv.conf etc/
-sudo chroot . chmod 755 -R etc/sudoers
-sudo chroot . chmod 755 -R etc/sudoers.d
+sudo sed -i -e 's/C.UTF8/"en_US.UTF-8"/' ./etc/env.d/02locale
+echo 'LC_COLLATE="C"' | sudo tee -a ./etc/env.d/02locale
+
+# Setup custom gentoo overlay
 sudo chroot . emerge -a n app-portage/layman
-sudo chroot . sed -i '/^#/!s/check_official .*/check_official : No/' /etc/layman/layman.cfg
+sudo sed -i '/^#/!s/check_official .*/check_official : No/' ./etc/layman/layman.cfg
 sudo chroot . layman -o https://raw.githubusercontent.com/ThatWeirdAndrew/gentoowsl-overlay/master/repositories.xml -f -a gentoowsl
-sudo chroot . layman-updater -R 
-sudo chroot . emerge -a n sys-apps/wslu
+sudo chroot . layman-updater -R
+sudo chroot . emerge -a n sys-apps/wslu sys-process/cronie
+sudo chroot . rc-update add cronie default
 
-sudo chroot . emerge -a n net-misc/netifrc
-sudo chroot . echo 'config_eth0="dhcp"' >> etc/conf.d/net
-sudo chroot . emerge -a n net-misc/dhcpcd
-sudo chroot . emerge -a n net-misc/iputils
-sudo rm var/cache/distfiles/*
+# Network things
+sudo chroot . emerge -a n net-misc/netifrc net-misc/dhcpcd net-misc/iputils
+echo 'config_eth0="dhcp"' | sudo tee -a ./etc/conf.d/net
 
-sudo umount ./{sys,proc}
-sudo tar cf - * -P | pv -s $(sudo du -sb ./ | awk '{print $1}') | sudo gzip > ../install.tar.gz
-sudo chown `id -un` ../install.tar.gz
-sudo mkdir targz
-sudo mv ../install.tar.gz targz
-echo "Build has completed. install.tar.gz file located in the rootfs/targz directory."
+# Unmount and compress rootfs
+sudo rm -rf ./var/cache/distfiles/*
+sudo umount -Rq ./{sys,proc,dev}
+sudo tar -zcpf ../rootfs.tar.gz *
+sudo chown $(id -un) ../rootfs.tar.gz
+mkdir ../out
+mv ../rootfs.tar.gz ../out
+echo "Build has completed. rootfs.tar.gz file located in the out directory."
